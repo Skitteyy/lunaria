83188cf92d88230130418da783d91704abeef991
Skitteyy <83917020+Skitteyy@users.noreply.github.com>
2023-07-20 00:18:25 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     client: {
-        token: 'MTEyMTAzNzM4OTQxOTI2MTk4NQ.GttnBU.opig7SQH96e3Mz8G_V2tnzn3sRinTNTw-PUN-g',
-        id: '1121037389419261985'
+        token: '',
+        id: ''
     },
     handler: {
         prefix: '*',
@@ -13,7 +13,7 @@ module.exports = {
             message: false
         },
         mongodb: {
-            uri: 'mongodb+srv://skittey:H9VSvc6ayDd7P5Ko@discordbot.uzwyegb.mongodb.net/'
+            uri: ''
         }
     }
 };
